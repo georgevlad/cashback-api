0e32798b2d4b41267bab53a45c130aa401a2172d
george.vlad <geo_vlad@yahoo.com>
2025-05-08 14:47:26 +0300

    changed homepage

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['CashbackGR API' => "0.1.0 alpha"];
 });
 
 require __DIR__.'/auth.php';
