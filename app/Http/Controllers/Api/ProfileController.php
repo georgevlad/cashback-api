ebe005246674610d30a75c7d85ea776e61faae2f
George Vlad <geo_vlad@yahoo.com>
2025-06-24 15:21:39 +0300

    Add user update API

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 class ProfileController extends Controller
 {
     public function show(Request $request)
@@ -12,6 +14,44 @@ class ProfileController extends Controller
         return ApiResponse::success(
             ['user' => $request->user()],
             __('messages.profile_retrieved')
+        );
+    }
+
+    public function update(Request $request)
+    {
+        $user = $request->user();
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|email|unique:users,email,' . $user->id,
+            'password' => 'sometimes|required|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return ApiResponse::error(
+                __('messages.validation_error'),
+                $validator->errors(),
+                422
+            );
+        }
+
+        if ($request->filled('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->filled('email')) {
+            $user->email = $request->email;
+        }
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return ApiResponse::success(
+            ['user' => $user],
+            __('messages.profile_updated')
         );
     }
 }
